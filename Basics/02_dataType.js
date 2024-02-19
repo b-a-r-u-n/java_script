4bee28d0f1210e5e94aee7b0237068e3181e1764
b-a-r-u-n <160399059+b-a-r-u-n@users.noreply.github.com>
2024-02-19 19:22:40 +0000

    datatypes

---
+++ Basics/02_dataType.js
@@ -0,0 +1,29 @@
+"use strict" ; //Treat all js code as newer version.
+
+// alert("Hello"); // We are using nodejs, not browser.
+
+console.log(3 +3) ;
+
+//Code readability should be high.
+
+console.log("Hello") ;
+
+let name = "Itachi" ;
+let age = 18 ;
+let isLoggedIn = false ;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof 1);
+console.log(typeof "1");
+console.log(typeof null); // IMPORTANT
+console.log(typeof undefined);
