c3ee0cf4b1c59b6f7d359f4669757abc362a5c7c
b-a-r-u-n <160399059+b-a-r-u-n@users.noreply.github.com>
2024-02-22 06:45:28 +0000

    conversion

--- Basics/03_conversionOperation.js
+++ Basics/03_conversionOperation.js
@@ -1,28 +1,44 @@
-let score = true ;
+// let score = true ;
 
-console.log(typeof score) ;
-console.log(typeof (score)) ;
+// console.log(typeof score) ;
+// console.log(typeof (score)) ;
 
-let valInNumber = Number(score) ;
+// let valInNumber = Number(score) ;
 
-console.log(typeof (valInNumber)) ;
+// console.log(typeof (valInNumber)) ;
 
-console.log(valInNumber) ;
+// console.log(valInNumber) ;
 
-// "33" => 33
-// "33abc" => NaN
-// true => 1 \\ false => 0
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1 \\ false => 0
 
-let isLoggedIn = 1 ;
+// let isLoggedIn = 1 ;
 
-let booleanIsLoggedIn = Boolean(isLoggedIn) ;
-console.log(booleanIsLoggedIn) ;
+// let booleanIsLoggedIn = Boolean(isLoggedIn) ;
+// console.log(booleanIsLoggedIn) ;
 
-// 1 => true // 0 => false
-// "" => false
-// "Name" => true 
+// // 1 => true // 0 => false
+// // "" => false
+// // "Name" => true 
 
-let someNumber = 33 ;
-let stringNumber = String(someNumber) ;
-console.log(stringNumber) ;
-console.log(typeof stringNumber) ;
+// let someNumber = 33 ;
+// let stringNumber = String(someNumber) ;
+// console.log(stringNumber) ;
+// console.log(typeof stringNumber) ;
+
+//************************* Operations *************************
+
+let val = 5;
+let negval = -(val);
+console.log(negval);
+
+console.log(2*2);
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2**3);
+console.log(2%2);
+
+
+let
