de81c716eb8f206b87ca7d1ec7d6baf69f00d008
b-a-r-u-n <160399059+b-a-r-u-n@users.noreply.github.com>
2024-03-01 14:13:46 +0000

    operation

--- Basics/03_conversionOperation.js
+++ Basics/03_conversionOperation.js
@@ -41,4 +41,25 @@ console.log(2**3);
 console.log(2%2);
 
 
-let
+let str1 = "Itachi" ;
+let str2 = " Uchiha" ;
+let str3 = console.log(str1 + str2);
+
+console.log("3" + 2);
+console.log(3 + "2");
+console.log(3 + "2" + "2");
+console.log(3 + 2 + "2");
+console.log("3" + 2 + 2);
+
+// console.log(true);            // CONFUSION CODE........
+// console.log(+true);           // CONFUSION CODE........
+// console.log(+"");             // CONFUSION CODE........
+
+// let num1, num2, num3 ;        // CONFUSION CODE........
+// num1 = num2 = num3 = 2 + 2    // CONFUSION CODE........
+
+let  gamecounter = 100 ;
+++gamecounter;
+console.log(gamecounter);
+gamecounter++;
+console.log(gamecounter);
