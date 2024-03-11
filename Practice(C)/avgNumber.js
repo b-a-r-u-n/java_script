c9b2c470d2b74d642381aec61683b21e7addf218
b-a-r-u-n <160399059+b-a-r-u-n@users.noreply.github.com>
2024-03-11 14:42:29 +0000

    practice

---
+++ Practice(C)/avgNumber.js
@@ -0,0 +1,8 @@
+let num1 = parseFloat(prompt("Enter first number:"));
+let num2 = parseFloat(prompt("Enter second number:"));
+let num3 = parseFloat(prompt("Enter third number:"));
+let num4 = parseFloat(prompt("Enter fourth number:"));
+let num5 = parseFloat(prompt("Enter fifth number:"));
+
+let avg = (num1+num2+num3+num4+num5)/5;
+console.log(`The average of 5 numbers is ${avg}.`);
