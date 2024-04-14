ce160ce9ebfc2c12245060489731c7abd658306b
b-a-r-u-n <160399059+b-a-r-u-n@users.noreply.github.com>
2024-04-14 07:37:35 +0000

    date in js

---
+++ Basics/07_datesinjs.js
@@ -0,0 +1,28 @@
+let date = new Date();
+
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+// console.log(typeof(date));
+
+// let createdDate = new Date(2024 , 0 , 11);
+// let createdDate = new Date("2024-01-01");
+let createdDate = new Date("01-01-2024");
+// console.log(createdDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp.toString());
+// console.log(createdDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getDate());
+
+// newDate.toLocaleString('default' , {
+//     weekday : "long",
+//     timeZone : 
+// })
